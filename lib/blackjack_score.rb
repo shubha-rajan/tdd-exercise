6e5bd07c28d3aaa6764567d792c74a4fc568f61d
Shubha Rajan <shubharajan94@gmail.com>
2019-02-21 08:12:54 -0800

    wrote tests and blackjack score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,37 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
 
 def blackjack_score(hand)
+  if hand.length > 5
+    raise ArgumentError, "You can't have more than 5 cards in a hand. Current hand size is #{hand.length}."
+  end
+  hand.each do |card|
+    if !VALID_CARDS.include?(card)
+      raise ArgumentError, "#{card} is not a valid card."
+    end
+  end
+  hand_scores = hand.map do |card|
+    case card
+    when "Queen", "King", "Jack"
+      10
+    else
+      card
+    end
+  end
 
+  score = 0
+
+  if hand_scores.include?(1)
+    score_without_aces = hand_scores.sum
+    if (score_without_aces + 10) <= 21
+      score = score_without_aces + 10
+    else
+      score = score_without_aces
+    end
+  else
+    score = hand_scores.sum
+  end
+
+  return score
 end
--- specs/blackjack_score_spec.rb
+++ specs/blackjack_score_spec.rb
@@ -1,15 +1,15 @@
-require 'minitest'
-require 'minitest/spec'
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/pride'
+require "minitest"
+require "minitest/spec"
+require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/pride"
 
-require_relative '../lib/blackjack_score'
+require_relative "../lib/blackjack_score"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe 'Blackjac Score' do
-  it 'can calculate the score for a pair of number cards' do
+describe "Blackjack Score" do
+  it "can calculate the score for a pair of number cards" do
 
     # Arrange
     hand = [3, 4]
@@ -18,26 +18,63 @@ describe 'Blackjac Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
-
+    expect(score).must_equal 7
   end
 
-  it 'facecards have values calculated correctly' do
+  it "facecards have values calculated correctly" do
+    # Arrange
+    hand = ["Queen", 4]
 
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert <-  You do this part!
+    expect(score).must_equal 14
   end
 
-  it 'calculates aces as 11 where it does not go over 21' do
+  it "calculates aces as 11 where it does not go over 21" do
+    # Arrange
+    hand = [2, 4, 1]
 
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 17
   end
 
-  it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+  it "calculates aces as 1, if an 11 would cause the score to go over 21" do
+    # Arrange
+    possible_hands = [[2, 4, 1], [10, 1], [10, 10, 1], [1, 1, 1, 1]]
 
+    # Act
+    scores = possible_hands.map { |hand| blackjack_score(hand) }
+
+    # Assert
+    expect(scores).must_equal [17, 21, 21, 14]
   end
 
-  it 'raises an ArgumentError for invalid cards' do
+  it "raises an ArgumentError for invalid cards" do
+    # Arrange
+    possible_hands = [[2, "apple", 1], [15, 1], [1, 100], [1, 1, "Bubblegum", 1]]
 
+    # Act + Assert
+    possible_hands.each do |time|
+      expect {
+        blackjack_score(time)
+      }.must_raise ArgumentError
+    end
   end
 
-  it 'raises an ArgumentError for more than 5 cards in the hand' do
+  it "raises an ArgumentError for more than 5 cards in the hand" do
+    # Arrange
+    possible_hands = [[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, "King", "Queen", "Jack"]]
 
+    # Act + Assert
+    possible_hands.each do |time|
+      expect {
+        blackjack_score(time)
+      }.must_raise ArgumentError
+    end
   end
 end
